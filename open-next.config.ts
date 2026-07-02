c63e02eec17c2a4359ff8718eaea69a82b74ebe1
ujjwalmohands-a11y <ujjwalmohands-a11y@users.noreply.github.com>
2026-07-02 16:58:05 +0530

    Update open-next.config.ts with required Cloudflare settings

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,26 @@
 import type { OpenNextConfig } from 'open-next/types/open-next';
 
 export default {
-  default: {},
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
 } satisfies OpenNextConfig;
