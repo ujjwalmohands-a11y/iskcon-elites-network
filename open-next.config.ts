cf6e2a63f85cd488317b5f9dbd5d2fc16c998b85
ujjwalmohands-a11y <ujjwalmohands-a11y@users.noreply.github.com>
2026-07-02 17:18:10 +0530

    Add @ts-ignore to suppress incorrect OpenNext config typing error

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,6 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
+// @ts-ignore - The Cloudflare config type is incomplete in this version of OpenNext
 export default defineCloudflareConfig({
   cloudflare: {
     useWorkerdCondition: false,
