937287d570cb6ecb12e19cff72e8968057dda4ff
ujjwalmohands-a11y <ujjwalmohands-a11y@users.noreply.github.com>
2026-07-02 17:15:31 +0530

    Disable useWorkerdCondition to fix pg-cloudflare resolution error natively

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,26 +1,7 @@
-const config = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-  edgeExternals: ["node:crypto"],
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-};
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default config;
+export default defineCloudflareConfig({
+  cloudflare: {
+    useWorkerdCondition: false,
+  },
+});
